b314738161bfba4a657f6799ce612d4956db3be1
benwalley <benwalleyorigami@gmail.com>
2018-05-10 19:53:50 -0500

    added link underlines and a little bit of transition

--- scripts/home.js
+++ scripts/home.js
@@ -4,6 +4,12 @@ var mainImage = $(".mainImage")[0];
 var imageButtons = $(".imageButtons")[0];
 var schedule = $(".homeSchedule")[0];
 
+var navLinks = $(".navLinkSpan");
+var navLink = $(".navLink");
+var currentPage = 0
+
+navLinks[currentPage].style.borderWidth = "2px"
+
 window.addEventListener("scroll", function(e){
 	if(window.scrollY >= 100 && navColored == false){
 		navbar.style.background = "#242222eb"
@@ -26,6 +32,28 @@ window.addEventListener("scroll", function(e){
 	mainImage.style.clipPath = "polygon(0 0, 100% 0%, 100% " + yClip + ", 0 "+ yClip +")"
 
 	// move schedule div
-	schedule.style.top = ((window.innerHeight*1.1) - (window.scrollY/3)) + "px"
+	schedule.style.top = ((window.innerHeight*1.1) - (window.scrollY/3.4)) + "px"
 
 })
+
+
+for(var i = 0; i < navLinks.length; i++){
+	navLink[i].addEventListener("mouseover", function(){
+		for (var q = 0; q < navLink.length; q++) {
+			navLinks[q].style.borderWidth = "0"
+		}
+		// add the underline class to the span inside the listener div
+		this.firstChild.style.borderWidth = "2px"
+	})
+}
+
+for (var i = 0; i < navLink.length; i++) {
+	navLink[i].addEventListener("mouseout", function(){
+		for (var q = 0; q < navLink.length; q++) {
+			navLinks[q].style.borderWidth = "0"
+		}
+		navLinks[currentPage].style.borderWidth = "2px"
+	})
+}
+	
+
